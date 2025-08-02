36acda6bf582b202b04bb92f9d25b7dc411cc1c0
Tercius <140905563+Terrcius@users.noreply.github.com>
2025-08-02 20:26:02 +0000

    Projeto Completo

---
+++ src/useCases/CreateUser/index.ts
@@ -0,0 +1,16 @@
+import { MailtrapMailProvider } from "../../providers/implementations/MailTrapMailProvider";
+import { PostgresUsersRepository } from "../../repositories/implementations/PostgresUsersRepository";
+import { CreateUserUseCase } from "./CreateUserUseCase";
+import { CreateUserController } from "./CreateUserController";
+
+const mailtrapMailProvider = new MailtrapMailProvider();
+const postgresUserRepository = new PostgresUsersRepository();
+
+const createUserUseCase = new CreateUserUseCase(
+  postgresUserRepository,
+  mailtrapMailProvider,
+);
+
+const createUserController = new CreateUserController(createUserUseCase);
+
+export { createUserUseCase, createUserController };
